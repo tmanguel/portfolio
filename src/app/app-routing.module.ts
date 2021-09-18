1da60ba9a175ebd9991d8928a1f8368e03d30c46
Tomas Manguel <tmanguel@mobilecomputing.com.ar>
2021-09-18 00:05:07 -0300

    certifications added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { CertificationsComponent } from './components/certifications/certifications.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     SkillsComponent,
+    CertificationsComponent,
   ],
   imports: [
     CommonModule,

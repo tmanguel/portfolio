3e0b2fc6682edbb5d03be7b1bb593d97a7827a67
Tomas Manguel <tmanguel@mobilecomputing.com.ar>
2021-09-21 15:38:21 -0300

    finished form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,24 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ContactService } from 'src/app/service/contact.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-  
   public contact: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { 
+  constructor(
+    private _formBuilder: FormBuilder,
+    private contactSrv: ContactService,
+    private _snackBar: MatSnackBar
+  ) {
     this.contact = this._formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required]
-    })
+      message: ['', Validators.required],
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public submitForm() {
+    let formData = new FormData();
+    formData.append("name", this.contact.get("name")!.value);
+    formData.append("email", this.contact.get("email")!.value);
+    formData.append("message", this.contact.get("message")!.value);
+
+    this.contactSrv.postMessage(formData).subscribe(
+      (response) => {
+        if (response["result"] == "success"){
+          console.log("salio todo perfecto")
+          this.openSnackBar('Email enviado con exito!', 'success')
+        }else{
+          console.error("hubo un error")
+          this.openSnackBar('Ha habido un error.', 'error')
+        }
+      },
+      (error) => {
+        console.error({error});
+      }
+    );
+    this.contact.reset();
   }
 
+  private openSnackBar(message:string, myClass:string){
+    this._snackBar.open(message, undefined, {
+      duration: 3000,
+      panelClass: myClass
+    });
+  }
 }
